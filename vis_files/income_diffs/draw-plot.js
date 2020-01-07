57ebb9f18e5a0973b02bad68d2f312d3d8bfc4ea
Ben Xiao <benjamin.y.xiao@gmail.com>
2020-01-06 18:54:58 -0800

    finished income diffs lines chart

--- vis_files/income_diffs/draw-plot.js
+++ vis_files/income_diffs/draw-plot.js
@@ -22,10 +22,10 @@ async function drawPlots() {
 	const width = 900
 	let dimensions = {
 		width: width,
-		height: width * 0.6,
+		height: width * 0.5,
 		margin: {
-			top: 35,
-			right: 80,
+			top: 100,
+			right: 150,
 			bottom: 50,
 			left: 50,
 		},
@@ -115,22 +115,79 @@ async function drawPlots() {
         .scale(yScale)
         .tickFormat(d3.format('.0s'))
         .ticks(5)
+        .tickSize(2)
     
     const yAxis = bounds.append('g')
         .call(yAxisGenerator)
+        .style('font-family', 'Monteserrat')
+        .attr('stroke-opacity', 0.35)
 
     const xAxisGenerator = d3.axisBottom()
         .scale(xScale)
         .ticks(5)
+        .tickSize(0)
     
     const xAxis = bounds.append('g')
         .call(xAxisGenerator)
             .style('transform', `translateY(${
                 dimensions.boundedHeight
             }px)`)
+        .style('font-family', 'Monteserrat')
+        .attr('stroke-opacity', 0.35)
 
-    // Annotations amd Title
-    
-    
+    // Line Labels
+
+    // Grab positions of last points of each line
+    let lastDate = dateAccessor(_.first(income_data, dateAccessor))
+    console.log(lastDate)
+
+    let lastWhite = whiteAccessor(_.first(income_data, whiteAccessor))
+    let lastBlack = blackAccessor(_.first(income_data, blackAccessor))
+    let lastHispanic = hispanicAccessor(_.first(income_data, hispanicAccessor))
+
+    const whiteLabel = d3.select('svg')
+        .append('text')
+        .attr('x', xScale(lastDate) + 55)
+        .attr('y', yScale(lastWhite) + 105)
+        .style('font-size', '13px')
+        .attr('font-family', 'Montserrat')
+        .text('White households')
+
+    const blackLabel = d3.select('svg')
+        .append('text')
+        .attr('x', xScale(lastDate) + 55)
+        .attr('y', yScale(lastBlack) + 105)
+        .style('font-size', '13px')
+        .style('font-family', 'Montserrat')
+        .style('font-weight', 'bold')
+        .text('Black households')
+
+    const hispanicLabel = d3.select('svg')
+        .append('text')
+        .attr('x', xScale(lastDate) + 55)
+        .attr('y', yScale(lastHispanic) + 105)
+        .style('font-size', '13px')
+        .style('font-family', 'Montserrat')
+        .style('font-weight', 'bold')
+        .text('Hispanic households') 
+
+    // Title and text
+
+    const chartTitleTop = d3.select('svg')
+        .append('text')
+        .attr('x', (width * 0.1))
+        .attr('y', (dimensions.height * 0.1))
+        .style('font-size', '20px')
+        .style('font-family', 'Montserrat')
+        .style('font-weight', 'bold')
+        .text('Black and Hispanic median household incomes')
+
+    const chartTitleBot = d3.select('svg')
+        .append('text')
+        .attr('x', (width * 0.1))
+        .attr('y', (dimensions.height * 0.1 + 20))
+        .style('font-size', '20px')
+        .style('font-family', 'Montserrat')
+        .text('have always been significiantly below the national median')
 }
 drawPlots()
